af75bc4ee64f35debf70211f7d844248ddcc660a
Mattew S <52506836+HK-Mattew@users.noreply.github.com>
2022-02-08 15:14:27 -0300

    Update custom.js

--- static/js/custom.js
+++ static/js/custom.js
@@ -1,16 +1,16 @@
 function getCurrencyQuote(currency){
-  var quote = null;
-  $.ajax({
+  return $.ajax({
       url: `https://api.binance.com/api/v3/avgPrice?symbol=${currency}USDT`,
       success: function(result){
         quote = parseFloat(result['price']);
       }
     });
-  return quote;
 };
 
 function convertUsdToCrypto(usd, to_currency){
-  return (usd / getCurrencyQuote(to_currency))
+  getCurrencyQuote(to_currency).then( response => {
+    return (usd / getCurrencyQuote(to_currency));
+  });
 };
 
 function convertCryptoToUsd(crypto_amount, crypto_currency){
